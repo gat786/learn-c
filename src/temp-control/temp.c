b08106d62365ec082bf3169be3e43769cabf1281
Ganesh Tiwari <ganesht049@gmail.com>
2024-10-26 09:21:40 +0530

    feat: fix float to int

--- src/temp-control/temp.c
+++ src/temp-control/temp.c
@@ -1,19 +1,20 @@
 #include <stdio.h>
 
 int to_celcius(int fahrenheit) {
-  printf("Temp in fahrenheit %d\n", fahrenheit);
+  printf("%8d", fahrenheit);
   // in c when dividing if both the numbers are int, then the
   // result will also be int, we need to make it float
   // to get the result we want.
-  int celcius = (fahrenheit - 32) * (5.0 / 9.0);
-  printf("Temperature in celcius %d\n", celcius);
+  float celcius = (fahrenheit - 32.0) * (5.0 / 9.0);
+  printf("%8.1f\n", celcius);
   return celcius;
 }
 
 int main() {
+  printf("%8s%8s\n","fah","cel");
   for (int i = 0; i <= 200; i++){
     int celcius = to_celcius(i);
-    printf("Value outside function %d\n", celcius);
+    // printf("Value outside function %d\n", celcius);
   }
 
   return 0;
