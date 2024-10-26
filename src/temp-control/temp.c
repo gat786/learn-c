40225d065676590ee157cd67f443a09d713384fd
Ganesh Tiwari <ganesht049@gmail.com>
2024-10-26 09:28:58 +0530

    feat: add fah to cel

--- src/temp-control/temp.c
+++ src/temp-control/temp.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
-int to_celcius(int fahrenheit) {
+float to_celcius(int fahrenheit) {
   printf("%8d", fahrenheit);
   // in c when dividing if both the numbers are int, then the
   // result will also be int, we need to make it float
@@ -10,10 +10,25 @@ int to_celcius(int fahrenheit) {
   return celcius;
 }
 
+float to_fahrenheit(int celcius) {
+  printf("%8d", celcius);
+  float fahrenheit = (celcius / (5.0/9.0)) + 32.0;
+  printf("%8.1f\n",fahrenheit);
+  return fahrenheit;
+}
+
+
+
 int main() {
   printf("%8s%8s\n","fah","cel");
   for (int i = 0; i <= 200; i++){
-    int celcius = to_celcius(i);
+    float celcius = to_celcius(i);
+    // printf("Value outside function %d\n", celcius);
+  }
+
+  printf("%8s%8s\n","cel","fah");
+  for (int i = 0; i <= 200; i++){
+    float fahrenheit = to_fahrenheit(i);
     // printf("Value outside function %d\n", celcius);
   }
 
